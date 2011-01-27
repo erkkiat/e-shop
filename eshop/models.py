3c9d943fe29a719cb1f13a71be707191819d46ee
Erkki Tapola <erkki.tapola@iki.fi>
2011-01-27 15:38:20 +0200

    Polishing for the admin GUI.

--- eshop/models.py
+++ eshop/models.py
@@ -4,6 +4,9 @@ from django.contrib.auth.models import User
 class Manufacturer(models.Model):
     name = models.CharField(max_length=80)
     
+    def __unicode__(self):
+        return self.name
+
 class Currency(models.Model):
     # Name, for example "British pound"
     name = models.CharField(max_length=20)
@@ -11,6 +14,9 @@ class Currency(models.Model):
     # Symbol, for example "EUR"
     symbol = models.CharField(max_length=3)
     
+    def __unicode__(self):
+        return self.name + ' (' + self.symbol + ')'
+
 class Product(models.Model):
     # Title for the product, tells the customer what the product is
     title = models.CharField(max_length=80)
@@ -21,13 +27,13 @@ class Product(models.Model):
     active = models.BooleanField(default=True)
       
     # Manufacturer of the product
-    manufacturer = models.ForeignKey(Manufacturer, null=True)
+    manufacturer = models.ForeignKey(Manufacturer, null=True, blank=True)
 
     # A one sentence description of the product
     short_description = models.CharField(max_length=100)
     
     # A full description to be shown in select views
-    long_description = models.TextField()
+    long_description = models.TextField(blank=True)
     
     # Price, up to 9999 units, two decimal places
     price = models.DecimalField(max_digits=4, decimal_places=2)
@@ -40,7 +46,8 @@ class Product(models.Model):
     # See http://docs.djangoproject.com/en/dev/ref/models/fields/#imagefield
 
     def __unicode__(self):
-        return self.title + ' costs ' + self.price
+        return self.title + ' (price ' + str(self.price) +\
+            ' ' + self.currency.symbol + ')'
 
 class Category(models.Model):
     """Product categories"""
